43a273346ce5817cc59056dd5648727839a873f4
a-nabokova <itcw2s@gmail.com>
2025-09-06 13:55:32 +0300

    toaster fix

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -62,6 +62,7 @@ function App() {
         {isModalOpen && selectedMovie && (
           <MovieModal movie={selectedMovie} onClose={closeModal} />
         )}
+         <Toaster position="top-center" />
       </div>
     </>
   )
